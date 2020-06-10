7a5fb574889ea3343aff51a76debca39b5f3da2e
Felipe Oliveira <flpdias14@gmail.com>
2020-06-10 17:05:06 -0300

    Adding springfox libraries

--- src/main/java/uname/group/vaqueiro/controller/AboioController.java
+++ src/main/java/uname/group/vaqueiro/controller/AboioController.java
@@ -1,5 +1,7 @@
 package uname.group.vaqueiro.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +11,14 @@ import uname.group.vaqueiro.model.Aboio;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+@Api(value = "Aboio")
 @Controller
 public class AboioController {
 
     private static final String template = "Ooohhh! %s";
     private final AtomicLong counter = new AtomicLong();
 
-
+    @ApiOperation(value = "Shouts an aboio")
     @GetMapping("/aboio")
     @ResponseBody
     public Aboio sayOohh(@RequestParam(name="name", required=false, defaultValue="Gado!") String name) {
---
+++ src/main/java/uname/group/vaqueiro/springfox/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package uname.group.vaqueiro.springfox;
+
+import io.swagger.annotations.Api;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import uname.group.vaqueiro.controller.AboioController;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {AboioController.class})
+public class SwaggerConfig {
+
+    @Bean
+    public Docket vaqueiroApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .build()
+                .apiInfo(metaData())
+                ;
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Vaqueiro REST API")
+                .description("\"Vaqueiro REST API for gados\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+}
